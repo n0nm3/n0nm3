775a929bc7b9951c8f92c235fe24e5a11b4f471c
n0nm3 <95082158+n0nm3@users.noreply.github.com>
2022-06-17 22:06:57 +0200

    Update aggreg.py

--- aggreg.py
+++ aggreg.py
@@ -1,12 +1,14 @@
 import feedparser as fp
 import sys 
 import time as tm
+import yaml
 
-def charge_urls(liste_url):
+
+def charge_urls(liste_url,rss_name):
     flux = []
     for element in liste_url:
         nv_flux = None      
-        nv_flux = fp.parse("http://"+element+'/rss.xml')        
+        nv_flux = fp.parse("http://"+element+"/"+rss_name)        
         verif = False
         if nv_flux["bozo"] == 0:
             verif = True
@@ -61,9 +63,15 @@ def genere_html(liste_evenements, chemin_html):
         guid = ""
         for i in uid[20:]:
             guid = guid + i
-        p_el = '<article>\n <header>\n <h2>'+ element["titre"] + '</h2>\n </header>\n  <p>from:' + element["serveur"] + '</p> <p>'+ element["date_publi"]+'</p><p>' + element["categorie"] + '</p> <p>' + guid +'</p> <p><a href="' + element["lien"] + '">' + element["lien"] + '</a></p><p>' + element["description"] + '</p>'
+        if element['categorie'] == "CRITICAL":
+            div = "critical"
+        elif element['categorie'] == "MAJOR":
+            div = "major"
+        elif element['categorie'] == "MINOR":
+            div = "minor"
+        p_el = '<article>\n <header>\n <h2>'+ element["titre"] + '</h2>\n </header>\n  <p>from:' + element["serveur"] + '</p> <p>'+ element["date_publi"]+'</p><div class="'+div+'">' + element["categorie"] + '</div> <p>' + guid +'</p> <p><a href="' + element["lien"] + '">' + element["lien"] + '</a></p><p>' + element["description"] + '</p></article>'
         page_element = page_element + p_el
-    page = '<html>\n <head>\n <meta charset="utf-8">\n <meta name="viewport" content="width=device-width, initial-scale=1">\n <title>Event log </title>\n <link rel="stylesheet" href="css/feed.css" type="text/css"> </head> <body> <article>\n <header>\n <h1>Events log</h1>\n </header>\n <p>'+time+'</p>\n'+ page_element +'</article>\n</body></html>'
+    page = '<html>\n <head>\n <meta charset="utf-8">\n <meta name="viewport" content="width=device-width, initial-scale=1">\n <title>Event log </title>\n <link rel="stylesheet" href="./style.css" type="text/css"> </head> <body> <article>\n <header>\n <h1>Events log</h1>\n </header>\n <p>'+time+'</p>\n'+ page_element +'\n</body></html>'
     x = open(chemin_html,"w")
     x.write(page)
     x.close()
@@ -71,12 +79,29 @@ def genere_html(liste_evenements, chemin_html):
 
 
 def main():
-    """liste_url = ["serveur1.net","toto.net"]"""
-    liste_url = sys.argv[1:] 
-    x = charge_urls(liste_url)
-    r = fusion_flux(liste_url, x)
-    h = genere_html(r,"/var/www/html/index.html")
-
+    liste_url = []
+    rss_name = ""
+    chemin_html = "" 
+    if len(sys.argv) <= 1:
+        liste_fichiers = []
+        fichier =("/etc/config.yml")
+        liste_fichiers.append(fichier)
+    else:
+        liste_fichiers = sys.argv[1:]
+    for fichier in liste_fichiers:
+        with open(fichier,'r') as fd:
+            data = yaml.safe_load(fd)
+        for element in data:
+            if element == "sources":
+                liste_url = data[element]
+            if element == "rss-name":
+                rss_name = data[element]
+            if element == "destination":
+                chemin_html = data[element]
+        x = charge_urls(liste_url,rss_name)
+        r = fusion_flux(liste_url, x)
+        h = genere_html(r,chemin_html)
+    
 if __name__ == "__main__":
     main()
 
